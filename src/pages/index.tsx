37c82bb3afe0694578dfbe0dfd54f811d33630aa
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-01-10 12:07:02 +0100

    Clean files and add css reset

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <Head>
+        <title>Nextjs starter</title>
+      </Head>
+      <h1>Hello World</h1>
+    </main>
+  );
+};
+
+export default Home;
