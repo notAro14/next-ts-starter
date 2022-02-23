67c2de5fc2ea9eec0cbc3bb555641c819a2eb031
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-02-24 00:48:12 +0100

    Add examples of variants

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head';
-import { ReactElement } from 'react';
+import { ReactElement, FC } from 'react';
 
 import Heading from 'shared/heading';
 import { useColorMode, useToggleColorMode } from 'theme/color-mode';
 import { NextPageWithLayout } from 'types';
 import GlobalLayout from 'shared/layouts/global-layout';
 import VStack from 'shared/v-stack';
-import BaseButton from 'shared/button/base-button';
+import BaseButton, {
+  ButtonVariant,
+  ButtonSize,
+} from 'shared/button/base-button';
+import HStack from 'shared/h-stack';
 
 const IndexPage: NextPageWithLayout = () => {
   return (
@@ -17,7 +21,18 @@ const IndexPage: NextPageWithLayout = () => {
       <Heading as='h1' color='primary' fontFamily='primary'>
         Hello world
       </Heading>
-      <ToggleThemeBtn />
+      <VStack gap={6}>
+        <HStack gap={3} alignItems='flex-end'>
+          <ToggleThemeBtn variant='filled.primary' />
+          <ToggleThemeBtn variant='filled.primary' size='medium' />
+          <ToggleThemeBtn variant='filled.primary' size='big' />
+        </HStack>
+        <HStack gap={3} alignItems='flex-end'>
+          <ToggleThemeBtn variant='outlined.primary' />
+          <ToggleThemeBtn variant='outlined.primary' size='medium' />
+          <ToggleThemeBtn variant='outlined.primary' size='big' />
+        </HStack>
+      </VStack>
     </>
   );
 };
@@ -30,10 +45,19 @@ IndexPage.getLayout = (page: ReactElement) => (
   </GlobalLayout>
 );
 
-const ToggleThemeBtn = () => {
+interface ToggleThemeBtnProps extends ButtonVariant, ButtonSize {}
+
+const ToggleThemeBtn: FC<ToggleThemeBtnProps> = ({
+  variant = 'filled',
+  size = 'small',
+}) => {
   const colorMode = useColorMode();
   const toggle = useToggleColorMode();
-  return <BaseButton onClick={toggle}>{colorMode}</BaseButton>;
+  return (
+    <BaseButton size={size} variant={variant} onClick={toggle}>
+      {colorMode}
+    </BaseButton>
+  );
 };
 
 export default IndexPage;
--- src/shared/button/base-button.ts
+++ src/shared/button/base-button.ts
@@ -3,23 +3,75 @@ import shouldForwardProp from '@styled-system/should-forward-prop';
 import css from '@styled-system/css';
 
 import { SystemStyleProps } from 'types/ui';
+import { variant } from 'styled-system';
 import { getSystemStyleProps, sx } from 'utils/ui';
 
-const BaseButton = styled('button', { shouldForwardProp })<SystemStyleProps>`
-  ${css({
+export type ButtonVariant = {
+  variant?: 'filled' | 'outlined' | 'filled.primary' | 'outlined.primary';
+};
+export type ButtonSize = { size?: 'small' | 'medium' | 'big' };
+
+const outlinedBase = {
+  backgroundColor: 'transparent',
+};
+const outlinedPrimary = {
+  ...outlinedBase,
+  color: 'accent',
+};
+const outlined = {
+  ...outlinedBase,
+  primary: outlinedPrimary,
+};
+
+const filledBase = {};
+const filled = {
+  ...filledBase,
+  primary: {
     backgroundColor: 'accent',
-    border: '1px solid',
     borderColor: 'accent',
-    borderRadius: 'sm',
     color: 'background',
+  },
+};
+
+const BaseButton = styled('button', { shouldForwardProp })<
+  SystemStyleProps & ButtonVariant & ButtonSize
+>`
+  ${css({
+    border: '1px solid',
+    borderRadius: 'sm',
     fontFamily: 'primary',
     fontWeight: 700,
-    py: 1,
-    px: 2,
+    lineHeight: '1',
     ':hover': {
       cursor: 'pointer',
     },
     textTransform: 'capitalize',
+  })}
+  ${variant({
+    variants: {
+      filled,
+      outlined,
+    },
+  })}
+  ${variant({
+    prop: 'size',
+    variants: {
+      small: {
+        py: 1,
+        px: 2,
+        fontSize: 'xs',
+      },
+      medium: {
+        py: 2,
+        px: 4,
+        fontSize: 'md',
+      },
+      big: {
+        py: 3,
+        px: 6,
+        fontSize: 'xl',
+      },
+    },
   })}
   ${getSystemStyleProps()}
   ${(props) => sx(props.sx)}
