55a63f1bbbb67db24f26be6794fdb33a9f61963a
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-05-29 01:17:34 +0200

    Add primitive components

---
+++ src/shared/flex/flex.ts
@@ -0,0 +1,44 @@
+import { styled } from "src/lib/stitches.config"
+import Box from "../box"
+
+const Flex = styled(Box, {
+  display: "flex",
+  variants: {
+    direction: {
+      column: {
+        flexDirection: "column",
+      },
+      row: {
+        flexDirection: "row",
+      },
+    },
+    gap: {
+      "1": {
+        gap: "$xxs",
+      },
+      "2": {
+        gap: "$xs",
+      },
+      "3": {
+        gap: "$sm",
+      },
+      "4": {
+        gap: "$base",
+      },
+      "5": {
+        gap: "$lg",
+      },
+      "6": {
+        gap: "$xl",
+      },
+      "7": {
+        gap: "$2xl",
+      },
+    },
+  },
+  defaultVariants: {
+    gap: 1,
+  },
+})
+
+export default Flex
