ab8ade9660253d9d61c335fb2f9f423190689470
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-08-09 23:45:26 +0200

    Update packages

--- src/features/user-list/user-list.helpers.ts
+++ src/features/user-list/user-list.helpers.ts
@@ -1,9 +1,9 @@
 import type { User } from "./user-list.types"
 
-export const fetchUsers = async () => {
+export const fetchUsers = async (): Promise<User[]> => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users")
   if (!res.ok) {
     throw new Error("Failed to get users")
   }
-  return res.json() as Promise<User[]>
+  return res.json()
 }
