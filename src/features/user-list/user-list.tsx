c7bd9d545161c9461abd305fc4f5d0117d3786da
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-08-12 22:40:57 +0200

    Add ErrorBoundary

--- src/features/user-list/user-list.tsx
+++ src/features/user-list/user-list.tsx
@@ -7,50 +7,48 @@ import Text from "src/ui/text"
 import { fetchUsers } from "./user-list.helpers"
 
 const UserList = () => {
-  const { status, data: users } = useQuery(["users"], fetchUsers)
+  const { data: users } = useQuery(["users"], fetchUsers, {
+    useErrorBoundary: true,
+  })
+  if (users === undefined)
+    return <Text role="progressbar">Loading users...</Text>
+
   return (
     <>
-      {status === "loading" && <Text role="progressbar">Loading users...</Text>}
-      {status === "error" && (
-        <Text role="alert" error>
-          Failed to get users
-        </Text>
-      )}
-      {status === "success" &&
-        users.map(({ id, username, phone, email }) => (
-          <Paper
-            as="li"
-            key={id}
-            css={{
-              height: "fit-content",
-              display: "flex",
-              flexDirection: "column",
-              gap: "$lg",
-            }}
-          >
-            <Heading as="h2" vibrant>
-              {username}
-            </Heading>
-            <Flex direction="column">
-              <Text
-                as="small"
-                css={{
-                  color: "$semantic-text-lo-gray",
-                }}
-              >
-                {phone}
-              </Text>
-              <Text
-                as="small"
-                css={{
-                  color: "$semantic-text-lo-gray",
-                }}
-              >
-                {email}
-              </Text>
-            </Flex>
-          </Paper>
-        ))}
+      {users.map(({ id, username, phone, email }) => (
+        <Paper
+          as="li"
+          key={id}
+          css={{
+            height: "fit-content",
+            display: "flex",
+            flexDirection: "column",
+            gap: "$lg",
+          }}
+        >
+          <Heading as="h2" vibrant>
+            {username}
+          </Heading>
+          <Flex direction="column">
+            <Text
+              as="small"
+              css={{
+                color: "$semantic-text-lo-gray",
+              }}
+            >
+              {phone}
+            </Text>
+            <Text
+              as="small"
+              css={{
+                color: "$semantic-text-lo-gray",
+              }}
+            >
+              {email}
+            </Text>
+          </Flex>
+        </Paper>
+      ))}
     </>
   )
 }
--- src/pages/user-list.tsx
+++ src/pages/user-list.tsx
@@ -1,5 +1,7 @@
 // VENDORS
 import type { NextPage } from "next"
+import { ErrorBoundary } from "react-error-boundary"
+import { useQueryErrorResetBoundary } from "@tanstack/react-query"
 // COMPONENTS
 import UserList from "src/features/user-list"
 import Box from "src/ui/box"
@@ -7,6 +9,8 @@ import Button from "src/ui/button"
 import Heading from "src/ui/heading"
 import SEO from "src/components/seo"
 import Link from "src/ui/link"
+import Text from "src/ui/text"
+import Flex from "src/ui/flex"
 // FUNCTIONS
 import useAutoAnimation from "src/utils/hooks/use-autoanimation"
 import useEnableOnce from "src/utils/hooks/use-enable-once"
@@ -31,6 +35,7 @@ const UserListContainer = styled("ul", {
 const UserListPage: NextPage = () => {
   const [isEnable, enableOnce] = useEnableOnce()
   const ulRef = useAutoAnimation<HTMLUListElement>()
+  const { reset } = useQueryErrorResetBoundary()
 
   return (
     <>
@@ -40,19 +45,31 @@ const UserListPage: NextPage = () => {
         User list
       </Heading>
       <Link href="/">Go to index page</Link>
-      <Box as="main">
-        <UserListContainer ref={ulRef}>
-          {isEnable && <UserList />}
-        </UserListContainer>
-        <Button
-          variant="outline"
-          onClick={() => {
-            enableOnce()
-          }}
-        >
-          Fetch users
-        </Button>
-      </Box>
+      <ErrorBoundary
+        onReset={reset}
+        fallbackRender={({ resetErrorBoundary }) => (
+          <Flex direction="column">
+            <Text role="alert" error>
+              Failed to get users
+            </Text>
+            <Button onClick={() => resetErrorBoundary()}>Try Again</Button>
+          </Flex>
+        )}
+      >
+        <Box as="main">
+          <UserListContainer ref={ulRef}>
+            {isEnable && <UserList />}
+          </UserListContainer>
+          <Button
+            variant="outline"
+            onClick={() => {
+              enableOnce()
+            }}
+          >
+            Fetch users
+          </Button>
+        </Box>
+      </ErrorBoundary>
     </>
   )
 }
