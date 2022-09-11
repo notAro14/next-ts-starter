e5a2425fbf28f17ae94e47032cbc58a3f02220f4
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-09-11 21:11:51 +0200

    Update go to home link

--- src/common/header/header.tsx
+++ src/common/header/header.tsx
@@ -1,14 +1,52 @@
 import ThemeSelect from "src/common/theme-select"
 
-import Text from "src/ui/text"
+import { HiHome } from "src/icons/hero-icon"
+
 import { Container, Nav } from "./header.styles"
+import { styled, theme } from "src/theme/stitches.config"
+import Link from "next/link"
+
+const IconButton = styled("button", {
+  padding: theme.space.xs,
+  borderRadius: theme.radii.md,
+  variants: {
+    variant: {
+      filled: {
+        border: "none",
+        backgroundColor: theme.colors.solid,
+        color: theme.colors["text-fg-white"],
+        "&:hover": {
+          backgroundColor: theme.colors["solid-hovered"],
+          boxShadow: theme.shadows.medium,
+          cursor: "pointer",
+        },
+      },
+      outline: {
+        border: `1px solid ${theme.colors.solid}`,
+        backgroundColor: "transparent",
+        color: theme.colors.solid,
+        "&:hover": {
+          cursor: "pointer",
+          border: `1px solid ${theme.colors["solid-hovered"]}`,
+          backgroundColor: "transparent",
+          boxShadow: "unset",
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    variant: "filled",
+  },
+})
 
 const Header = () => (
   <Container>
     <Nav>
-      <Text vibrant fontSize="lg">
-        Next TS starter
-      </Text>
+      <Link href="/" passHref>
+        <IconButton as="a" aria-label="Go to home" title="Home">
+          <HiHome />
+        </IconButton>
+      </Link>
       <ThemeSelect />
     </Nav>
   </Container>
---
+++ src/icons/hero-icon/index.ts
@@ -0,0 +1,1 @@
+export { default as HiHome } from "./hi-home"
