0dd9267af60f52fe4345aaf02d8a85fafdc41e07
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-09-11 18:08:34 +0200

    Refactor Header

---
+++ src/common/header/header.tsx
@@ -0,0 +1,11 @@
+import ThemeSelect from "../theme-select"
+
+const Header = () => (
+  <header>
+    <nav>
+      <ThemeSelect />
+    </nav>
+  </header>
+)
+
+export default Header
--- src/common/layout/layout.tsx
+++ src/common/layout/layout.tsx
@@ -2,23 +2,16 @@ import type { FC } from "react"
 
 import { styled, theme } from "src/theme/stitches.config"
 
-import Footer from "../footer"
-import ThemeSelect from "src/common/theme-select"
+import Footer from "src/common/footer"
+import Header from "src/common/header"
 
 const AppWrapper = styled("section", {
   display: "flex",
   flexDirection: "column",
   minHeight: "100%",
+  position: "relative",
   backgroundColor: theme.colors.bg,
 })
-
-const Header = () => (
-  <header>
-    <nav>
-      <ThemeSelect />
-    </nav>
-  </header>
-)
 
 interface Props {
   children: JSX.Element
