31a3d441e01f4e849d2db1e2f92569cb0a05b0e5
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-09-20 00:50:45 +0200

    Make defaul fonts as system fonts

--- src/styles/theme/tokens/fonts.ts
+++ src/styles/theme/tokens/fonts.ts
@@ -1,4 +1,13 @@
+const systemSans =
+  "-apple-system, BlinkMacSystemFont, 'Segoe UI','Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans','Helvetica Neue', sans-serif"
+const systemMono =
+  "Menlo, Monaco, 'Lucida Console', 'Liberation Mono', 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono','Courier New', monospace"
+
 export const fonts = {
-  sans: '"Metropolis"',
-  mono: '"Fira Code"',
+  metropolis: '"Metropolis"',
+  "fira-code": '"Fira Code"',
+  "system-sans": systemSans,
+  "system-mono": systemMono,
+  sans: systemSans,
+  mono: systemMono,
 }
