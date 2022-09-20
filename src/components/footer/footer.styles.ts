3ef8145d847831e770cabf38fe204ebe8567b4b2
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-09-20 02:03:43 +0200

    Update footer bg color

--- src/components/footer/footer.styles.ts
+++ src/components/footer/footer.styles.ts
@@ -2,8 +2,10 @@ import { styled, theme } from "src/styles/theme/stitches.config"
 
 export const Container = styled("footer", {
   marginTop: "auto",
-  backgroundColor: theme.colors["bg-subtle"],
+  backgroundColor: theme.colors.bg,
   display: "grid",
   placeItems: "center",
   height: 100,
+  borderTop: "1px solid",
+  borderTopColor: theme.colors["border-subtle"],
 })
