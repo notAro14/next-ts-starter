5228ebc4e5d36cd7e230917e6c1469b44be0d860
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-09-20 02:39:26 +0200

    Overengineer footer styles

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,15 +1,39 @@
+import { ExternalLinkIcon } from "@radix-ui/react-icons"
 import type { FC } from "react"
 
 import Link from "src/components/common/link"
 import Text from "src/components/common/text"
+import { theme } from "src/styles/theme/stitches.config"
 
 import { Container } from "./footer.styles"
 
 const Footer: FC = () => {
   return (
     <Container>
-      <Text fontSize="md">
-        Made by <Link href="htpps://github.com/notAro14">Aro Andriamaro</Link>
+      <Text
+        fontSize="md"
+        css={{
+          display: "flex",
+          gap: theme.space.xxs,
+          alignItems: "baseline",
+        }}
+      >
+        <Text as="span">Made by</Text>
+        <Link
+          href="https://github.com/notAro14"
+          target="_blank"
+          rel="noreferrer"
+          css={{
+            display: "flex",
+            alignItems: "center",
+            gap: theme.space.xxs,
+          }}
+        >
+          <Text as="span" css={{ color: "inherit" }}>
+            Aro Andriamaro
+          </Text>
+          <ExternalLinkIcon />
+        </Link>
       </Text>
     </Container>
   )
