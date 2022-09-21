d010a45e5d64fe83ba84112a9ef4c434146e742e
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-09-22 00:16:46 +0200

    Integrate prisma adapter for next auth

--- src/pages/api/auth/[...nextauth].page.ts
+++ src/pages/api/auth/[...nextauth].page.ts
@@ -1,20 +1,28 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
-const { GITHUB_ID, GITHUB_SECRET } = process.env
+const { GITHUB_ID, GITHUB_SECRET, NEXTAUTH_SECRET } = process.env
+
+const prisma = new PrismaClient()
 
 if (typeof GITHUB_ID === "undefined")
   throw new Error("GITHUB_ID is not defined")
 if (typeof GITHUB_SECRET === "undefined")
   throw new Error("GITHUB_SECRET is not defined")
+if (typeof NEXTAUTH_SECRET === "undefined")
+  throw new Error("NEXTAUTH_SECRET is not defined")
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: GITHUB_ID,
       clientSecret: GITHUB_SECRET,
     }),
   ],
+  secret: NEXTAUTH_SECRET,
 }
 
 export default NextAuth(authOptions)
