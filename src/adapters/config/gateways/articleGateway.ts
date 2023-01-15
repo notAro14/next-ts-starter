fc85f6736255fcbe1c6e6676d57f9446f513631a
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-01-15 20:56:31 +0100

    Use production gateway

--- src/adapters/config/gateways/articleGateway.ts
+++ src/adapters/config/gateways/articleGateway.ts
@@ -1,10 +1,12 @@
-import { InMemoryArticleGateway } from "src/adapters/secondary/gateways/inMemoryArticleGateway";
-import type { Article } from "src/core/entities";
+// import { InMemoryArticleGateway } from "src/adapters/secondary/gateways/inMemoryArticleGateway";
+// import type { Article } from "src/core/entities";
+import { ProdArticleGateway } from "src/adapters/secondary/gateways/prodArticleGateway";
 
-const FAKE_ARTICLES: Article[] = [
-  { id: "456abc", title: "TDD in frontend world" },
-  { id: "123abc", title: "Clean architecture in React" },
-];
+// const FAKE_ARTICLES: Article[] = [
+//   { id: "456abc", title: "TDD in frontend world" },
+//   { id: "123abc", title: "Clean architecture in React" },
+// ];
 
-export const articleGateway = new InMemoryArticleGateway();
-articleGateway.init = FAKE_ARTICLES;
+// export const articleGateway = new InMemoryArticleGateway();
+// articleGateway.init = FAKE_ARTICLES;
+export const articleGateway = new ProdArticleGateway();
